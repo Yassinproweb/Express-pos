aec42e5ca4acba82da9ae22c35ca6d0e5650a57e
Yassinproweb <yassinkatungi67@gmail.com>
2025-06-05 07:26:07 +0300

    changed font to Cairo

--- db/sqlite.js
+++ db/sqlite.js
@@ -7,5 +7,40 @@ export async function initDB() {
     driver: sqlite3.Database
   });
 
+  const productQuery = `
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      price REAL,
+      stock INTEGER,
+      image TEXT
+    );
+  `;
 
+  const tableQuery = `
+    CREATE TABLE IF NOT EXISTS tables (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      booked BOOLEAN,
+      capacity INTEGER
+    );
+  `;
+
+  const orderQuery = `
+    CREATE TABLE IF NOT EXISTS orders (
+      id TEXT PRIMARY KEY,
+      table_id TEXT, 
+      status TEXT, 
+      created_at TEXT, 
+      total_price REAL
+    );
+  `;
+
+  const boughtQuery = `
+    CREATE TABLE IF NOT EXISTS bought_items (
+      order_id TEXT, 
+      product_name TEXT,
+      quantity INTEGER,
+      price REAL
+    );
+  `;
 };
